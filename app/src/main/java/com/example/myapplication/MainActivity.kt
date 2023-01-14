808dd56d9b97639b5385a26544b16b7e0dd04d4c
leydev <wesley.waaraujo@gmail.com>
2023-01-14 14:01:54 -0300

    fix code and update style

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -6,84 +6,178 @@ import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
-import kotlin.experimental.and
+import java.util.Timer
+import kotlin.concurrent.schedule
 import kotlin.experimental.xor
 
 class MainActivity : AppCompatActivity() {
-    var tapTimes = 0
-    var playerTurn: Byte = 0b0;
-    var cells = byteArrayOf(
-        0b000000000, 0b000000000, 0b000000000,
-        0b000000000, 0b000000000, 0b000000000,
-        0b000000000, 0b000000000, 0b000000000,
+    private var playerTurn: Byte = 0b0
+    private var player0 = byteArrayOf(
+        0b0, 0b0, 0b0,
+        0b0, 0b0, 0b0,
+        0b0, 0b0, 0b0,
+    )
+    private var player1 = byteArrayOf(
+        0b0, 0b0, 0b0,
+        0b0, 0b0, 0b0,
+        0b0, 0b0, 0b0,
+    )
+
+    private val wins = byteArrayOf(
+        0b111000000.toByte(),
+        0b000111000.toByte(),
+        0b000000111.toByte(),
+        0b100100100.toByte(),
+        0b010010010.toByte(),
+        0b001001001.toByte(),
+        0b100010001.toByte(),
+        0b001010100.toByte()
     )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var display = findViewById<TextView>(R.id.display)
+        val display = findViewById<TextView>(R.id.display)
         display.text = "Tapped 0"
+        resetCells()
     }
 
-    fun setPlayerTurn() {
-        playerTurn = playerTurn.xor(0b1);
+    private fun resetCells () {
+        findViewById<TextView>(R.id.button0).text = ""
+        findViewById<TextView>(R.id.button1).text = ""
+        findViewById<TextView>(R.id.button2).text = ""
+        findViewById<TextView>(R.id.button3).text = ""
+        findViewById<TextView>(R.id.button4).text = ""
+        findViewById<TextView>(R.id.button5).text = ""
+        findViewById<TextView>(R.id.button6).text = ""
+        findViewById<TextView>(R.id.button7).text = ""
+        findViewById<TextView>(R.id.button8).text = ""
     }
 
-    fun getPlayerMarker(turn: Byte): String {
-        return if (turn === 0b0.toByte()) "O" else "X"
+    private fun setPlayerTurn() {
+        playerTurn = playerTurn.xor(0b1)
+    }
+
+    private fun getPlayerMarker(turn: Byte): String {
+        return if (turn == 0b0.toByte()) "O" else "X"
+    }
+
+    private fun setCell(index: Int) {
+        if (playerTurn == 0b0.toByte())
+            player0[index] = 0b1
+        else
+            player1[index] = 0b1
+    }
+
+    private fun checkWinnerOrFinish() {
+        var win = false
+        val joined0 = player0.reduce{ acc, curr -> ((acc.toInt() shl 1) or curr.toInt()).toByte() }
+        val joined1 = player1.reduce{ acc, curr -> ((acc.toInt() shl 1) or curr.toInt()).toByte() }
+        val joined = if (playerTurn == 0b0.toByte()) joined0 else joined1
+
+
+        for (check in wins) {
+            if (check == joined) {
+                win = true
+                break
+            }
+        }
+
+        if (win) {
+            val display = findViewById<TextView>(R.id.display)
+            display.text = "${getPlayerMarker(playerTurn)} won"
+            Timer().schedule(3000) {
+                resetCells()
+            }
+        } else if ((joined1.toInt() or joined0.toInt()) == -1) {
+            val display = findViewById<TextView>(R.id.display)
+            display.text = "Deu velha"
+            Timer().schedule(3000) {
+                resetCells()
+            }
+        } else setPlayerTurn()
     }
 
     fun onClick0(view: View) {
-        var button = findViewById<TextView>(R.id.button0) as Button
+        val button = view as Button
+        if (button.text != "") return
+
+        setCell(0)
         button.text = getPlayerMarker(playerTurn)
-        setPlayerTurn()
+        checkWinnerOrFinish()
     }
 
     fun onClick1(view: View) {
-        var button = findViewById<TextView>(R.id.button1) as Button
+        val button = view as Button
+        if (button.text != "") return
+
+        setCell(1)
         button.text = getPlayerMarker(playerTurn)
-        setPlayerTurn()
+        checkWinnerOrFinish()
     }
 
     fun onClick2(view: View) {
-        var button = findViewById<TextView>(R.id.button2) as Button
+        val button = view as Button
+        if (button.text != "") return
+
+        setCell(2)
         button.text = getPlayerMarker(playerTurn)
-        setPlayerTurn()
+        checkWinnerOrFinish()
     }
 
     fun onClick3(view: View) {
-        var button = findViewById<TextView>(R.id.button3) as Button
+        val button = view as Button
+        if (button.text != "") return
+
+        setCell(3)
         button.text = getPlayerMarker(playerTurn)
-        setPlayerTurn()
+        checkWinnerOrFinish()
     }
 
     fun onClick4(view: View) {
-        var button = findViewById<TextView>(R.id.button4) as Button
+        val button = view as Button
+        if (button.text != "") return
+
+        setCell(4)
         button.text = getPlayerMarker(playerTurn)
-        setPlayerTurn()
+        checkWinnerOrFinish()
     }
 
     fun onClick5(view: View) {
-        var button = findViewById<TextView>(R.id.button5) as Button
+        val button = view as Button
+        if (button.text != "") return
+
+        setCell(5)
         button.text = getPlayerMarker(playerTurn)
-        setPlayerTurn()
+        checkWinnerOrFinish()
     }
 
     fun onClick6(view: View) {
-        var button = findViewById<TextView>(R.id.button6) as Button
+        val button = view as Button
+        if (button.text != "") return
+
+        setCell(6)
         button.text = getPlayerMarker(playerTurn)
-        setPlayerTurn()
+        checkWinnerOrFinish()
     }
 
     fun onClick7(view: View) {
-        var button = findViewById<TextView>(R.id.button7) as Button
+        val button = view as Button
+        if (button.text != "") return
+
+        setCell(7)
         button.text = getPlayerMarker(playerTurn)
-        setPlayerTurn()
+        checkWinnerOrFinish()
     }
 
     fun onClick8(view: View) {
-        var button = findViewById<TextView>(R.id.button8) as Button
+        val button = view as Button
+        if (button.text != "") return
+
+        setCell(8)
         button.text = getPlayerMarker(playerTurn)
-        setPlayerTurn()
+        checkWinnerOrFinish()
     }
 }
+
+
